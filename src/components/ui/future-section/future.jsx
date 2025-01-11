eabc801021c6255d0b310568922d9b702d952a4c
karimoffabdulatif <akarimoff001@gmail.com>
2025-01-11 15:32:10 +0500

    .

--- src/components/ui/future-section/future.jsx
+++ src/components/ui/future-section/future.jsx
@@ -1,11 +1,12 @@
 import Home from "../../../../public/Home-icon.svg";
 import Moto from "../../../../public/food-develery.svg";
 import Smile from "../../../../public/smile-icon.svg";
+
 const Future = () => {
   return (
-    <section className="Future container mx-auto flex justify-between  py-4 px-4 md:px-[150px]">
+    <section className="Future pt-[102px] container mx-auto flex justify-between py-4 px-4 md:px-[150px]">
       <div className="flex flex-col items-center justify-center pl-[40px]">
-        <div className="flex flex-col items-center justify-center ">
+        <div className="flex flex-col items-center justify-center">
           <div className="text-center">
             <p className="text-orange-500 uppercase text-base tracking-widest">
               Features
@@ -15,7 +16,7 @@ const Future = () => {
             </h2>
           </div>
           <div className="grid grid-cols-3 gap-8 mt-16 w-full max-w-6xl">
-            <div className="text-center">
+            <div className="flex flex-col items-center justify-center text-center">
               <div className="relative flex justify-center items-center w-[88px] h-[88px]">
                 <div className="absolute inset-0 bg-text-green-future rounded-full opacity-25"></div>
                 <img
@@ -24,18 +25,17 @@ const Future = () => {
                   className="relative w-14 h-14 z-10"
                 />
               </div>
-
               <h3 className="font-bold mt-4">Quality Food</h3>
               <p className="text-gray-600 mt-2">
                 It can be a very secure path to earn good money and make you
                 very successful creative entrepreneur.
               </p>
             </div>
-            <div className="text-center">
+            <div className="flex flex-col items-center justify-center text-center">
               <div className="relative flex justify-center items-center w-[88px] h-[88px]">
-                <div className="absolute inset-0 bg-text-green-future rounded-full opacity-25"></div>
+                <div className="absolute inset-0 bg-text-red-future rounded-full opacity-25"></div>
                 <img
-                  src={Home}
+                  src={Moto}
                   alt="Quality Food"
                   className="relative w-14 h-14 z-10"
                 />
@@ -46,11 +46,11 @@ const Future = () => {
                 very successful creative entrepreneur.
               </p>
             </div>
-            <div className="text-center">
+            <div className="flex flex-col items-center justify-center text-center">
               <div className="relative flex justify-center items-center w-[88px] h-[88px]">
-                <div className="absolute inset-0 bg-text-green-future rounded-full opacity-25"></div>
+                <div className="absolute inset-0 bg-text-yellow rounded-full opacity-25"></div>
                 <img
-                  src={Home}
+                  src={Smile}
                   alt="Quality Food"
                   className="relative w-14 h-14 z-10"
                 />
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ export default {
         'gray-dark': '#252525',
         'text-footer': '#FFFFFF',
         'text-green-future': '#8FA632',
+        'text-red-future': '#E64D2E',
+        'text-yellow': '#DC780B',
       },
       spacing: {
         '41': '41px', 
